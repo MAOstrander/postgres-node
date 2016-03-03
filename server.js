d833b94b74244a89e3572046251e9d2d5ef8e3d4
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-03 09:23:35 -0600

    postgres goodness

--- server.js
+++ server.js
@@ -1,60 +1,104 @@
 'use strict';
 
 const express = require('express');
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('postgres://localhost:5432/nodegresql');
-const models = require('./models/')
+
+const models = require('./models/');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-  res.send('Hey, this is crazy')
+  res.send({
+    status: 'Success'
+  });
 });
 
 app.get('/genres', (req, res) => {
-  models.Genre
-  .findAll()
-  .then( (genres) => {
-    res.send(genres);
-  })
+  models.Genre.findAll()
+    .then(genres => res.send(genres));
 });
 
 app.get('/mediatypes', (req, res) => {
-  models.MediaType
-  .findAll()
-  .then( (types) => {
-    res.send(types);
-  })
+  models.MediaType.findAll()
+    .then(types => res.send(types));
 });
 
 app.get('/artists', (req, res) => {
-  models.Artist
-  .findAll()
-  .then( (singer) => {
-    res.send(singer);
-  })
+  models.Artist.findAll()
+    .then(artists => res.send(artists));
 });
 
 app.get('/playlists', (req, res) => {
-  models.Playlist
-  .findAll()
-  .then( (tracks) => {
-    res.send(tracks);
-  })
+  models.Playlist.findAll()
+    .then(playlists => res.send(playlists));
 });
 
 app.get('/albums', (req, res) => {
   models.Album.findAll({
-      attributes: {exclude: ['ArtistId']},
+      attributes: ['AlbumId', 'Title'],
       include: {
         model: models.Artist,
-        attributes: {exclude: ['ArtistId']}
+        attributes: ['Name']
       }
     })
-    .then(cds => res.send(cds));
+    .then(albums => res.send(albums));
+});
+
+app.get('/invoices', (req, res) => {
+  models.Invoice.findAll({
+      attributes: { exclude: 'CustomerId' },
+      include: {
+        model: models.Customer,
+        attributes: { exclude: [
+          'CustomerId',
+          'SupportRepId'
+        ]}
+      }
+    })
+    .then(invoices => res.send(invoices));
+});
+
+app.get('/invoices/:id', (req, res) => {
+  models.Invoice.findOne({
+      where: {
+        InvoiceId: req.params.id
+      }
+    })
+    .then(invoice => res.send(invoice));
+});
+
+app.get('/invoices/:id/customer', (req, res) => {
+  models.Invoice.findOne({
+      where: {
+        InvoiceId: req.params.id
+      }
+    })
+    .then(invoice => invoice.getCustomer())
+    .then(customer => res.send(customer));
+});
+
+app.get('/customers', (req, res) => {
+  models.Customer.findAll()
+    .then(customers => res.send(customers));
+});
+
+app.get('/customers/:id', (req, res) => {
+  models.Customer.findOne({
+    where: {
+      CustomerId: req.params.id
+    }
+  }).then(customer => res.send(customer));
+});
+
+app.get('/customers/:id/invoices', (req, res) => {
+  models.Customer.findOne({
+    where: {
+      CustomerId: req.params.id
+    },
+  })
+  .then(customer => customer.getInvoices())
+  .then(invoices => res.send(invoices));
 });
 
 app.listen(PORT, () => {
